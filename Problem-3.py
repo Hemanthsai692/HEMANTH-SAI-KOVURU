7ca4b2b2066692330a713c3a63529276d6620df0
Hemanthsai692 <hemanthsaikovuru@gmail.com>
2025-11-20 11:51:43 +0530

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,1 +1,16 @@
+a = int(input("Enter a number: "))
+
+# If even, reduce by 1 to nearest odd
+if a % 2 == 0:
+    limit = a - 1
+else:
+    limit = a
+
+result = []
+
+# Generate odd numbers 1, 3, 5, ... <= limit
+for num in range(1, limit + 1, 2):
+    result.append(num)
+
+print(result)
 
