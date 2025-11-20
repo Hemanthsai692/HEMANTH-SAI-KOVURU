ccaf55c3bd168e727120f2ce768442b1cacb74f0
Hemanthsai692 <hemanthsaikovuru@gmail.com>
2025-11-20 12:43:17 +0530

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,18 +1,13 @@
 a = int(input("Enter a number: "))
-
-# Determine how many odd numbers to print
 if a % 2 == 0:
     count = a - 1
 else:
     count = a
-
 result = []
 value = 1
-
 for i in range(count):
     result.append(value)
     value += 2
-
 print(result)
 
 
